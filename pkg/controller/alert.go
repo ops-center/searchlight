fc0be6d2a3b69eafc2a0ce4cf643fa29a584a8c5
Mir Shahriar Sabuj <aerokite@users.noreply.github.com>
2017-02-09 13:08:52 +0600

    Added new EventReason "NoIcingaObjectCreated" while Kube Object is NotFound. (#19)
    If alert is set on Invalid Kubernetes object, Controller will fail to detect Kube object. Then it will create an Event with EventReason "NoIcingaObjectCreated".

--- pkg/controller/alert.go
+++ pkg/controller/alert.go
@@ -79,8 +79,13 @@ func (b *IcingaController) handleAlert(e *events.Event) error {
 		event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.CreatingIcingaObjects)
 
 		if err := b.IsObjectExists(); err != nil {
-			event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.FailedToCreateIcingaObjects, err.Error())
-			return errors.New().WithCause(err).Internal()
+			if errors.IsNotFound(err) {
+				event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.NoIcingaObjectCreated, err.Error())
+				return nil
+			} else {
+				event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.FailedToCreateIcingaObjects, err.Error())
+				return errors.New().WithCause(err).Internal()
+			}
 		}
 		if err := b.Create(); err != nil {
 			event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.FailedToCreateIcingaObjects, err.Error())
@@ -104,8 +109,13 @@ func (b *IcingaController) handleAlert(e *events.Event) error {
 		event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.UpdatingIcingaObjects)
 
 		if err := b.IsObjectExists(); err != nil {
-			event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.FailedToUpdateIcingaObjects, err.Error())
-			return errors.New().WithCause(err).Internal()
+			if errors.IsNotFound(err) {
+				event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.NoIcingaObjectCreated, err.Error())
+				return nil
+			} else {
+				event.CreateAlertEvent(b.ctx.KubeClient, b.ctx.Resource, types.FailedToCreateIcingaObjects, err.Error())
+				return errors.New().WithCause(err).Internal()
+			}
 		}
 
 		if err := b.Update(); err != nil {
