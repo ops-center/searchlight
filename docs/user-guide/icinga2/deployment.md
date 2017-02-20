7c13966338235717bba922133a203df8c93841a7
Mir Shahriar <aerokite@users.noreply.github.com>
2017-02-20 16:20:26 +0600

    Support Slack as notification target (#33)

--- docs/user-guide/icinga2/deployment.md
+++ docs/user-guide/icinga2/deployment.md
@@ -53,6 +53,7 @@ And also we need to add some keys for notifier in Icinga2 Secret. We are current
 2. [Mailgun](../notifier/mailgun.md#set-environment-variables)
 3. [SMTP](../notifier/smtp.md#set-environment-variables)
 4. [Twilio](../notifier/twilio.md#set-environment-variables)
+5. [Slack](../notifier/slack.md#set-environment-variables)
 
 If we don't set keys for notifier, notifications will be ignored.
 
@@ -77,6 +78,7 @@ To use notifier we need to set some environment variables. See following links t
 2. [Mailgun](../notifier/mailgun.md#configure)
 3. [SMTP](../notifier/smtp.md#configure)
 4. [Twilio](../notifier/twilio.md#configure)
+4. [Slack](../notifier/slack.md#configure)
 
 ```sh
 # Create Deployment
---
+++ plugins/notifier/driver/slack/chat.go
@@ -0,0 +1,38 @@
+package slack
+
+import (
+	"strings"
+
+	api "github.com/appscode/api/kubernetes/v1beta1"
+	"github.com/appscode/go-notify/slack"
+	"github.com/appscode/searchlight/plugins/notifier/driver"
+	"github.com/appscode/searchlight/plugins/notifier/driver/extpoints"
+)
+
+type biblio struct{}
+
+func init() {
+	extpoints.Drivers.Register(new(biblio), slack.Uid)
+}
+
+func (b *biblio) Notify(req *api.IncidentNotifyRequest) error {
+	parts := strings.Split(req.HostName, "@")
+	namespace := parts[1]
+
+	alert, err := driver.GetAlertInfo(namespace, req.KubernetesAlertName)
+	if err != nil {
+		return err
+	}
+
+	chatBody, err := driver.RenderSMS(alert, req)
+	if err != nil {
+		return err
+	}
+
+	client, err := slack.Default()
+	if err != nil {
+		return err
+	}
+
+	return client.WithBody(chatBody).Send()
+}
