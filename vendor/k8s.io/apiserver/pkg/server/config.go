76714864a47f8853da6871feea2c484ba4e9dc4b
Tamal Saha <tamal@appscode.com>
2019-01-03 13:35:56 -0800

    Pin go-ini dependency (#434)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- vendor/k8s.io/apiserver/pkg/server/config.go
+++ vendor/k8s.io/apiserver/pkg/server/config.go
@@ -207,6 +207,9 @@ type SecureServingInfo struct {
 	// allowed to be in SNICerts.
 	Cert *tls.Certificate
 
+	// CertFile is the file containing the main server cert.
+	CertFile string
+
 	// SNICerts are the TLS certificates by name used for SNI.
 	SNICerts map[string]*tls.Certificate
 
@@ -415,6 +418,14 @@ func (c *Config) Complete(informers informers.SharedInformerFactory) CompletedCo
 
 	if c.RequestInfoResolver == nil {
 		c.RequestInfoResolver = NewRequestInfoResolver(c)
+	}
+
+	if c.SecureServing.CertFile != "" {
+		certChecker, err := healthz.NewCertHealthz(c.SecureServing.CertFile)
+		if err != nil {
+			glog.Fatalf("failed to create certificate checker. Reason: %v", err)
+		}
+		c.HealthzChecks = append(c.HealthzChecks, certChecker)
 	}
 
 	return CompletedConfig{&completedConfig{c, informers}}
